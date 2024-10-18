1b69a3fd0de4c0f104dc71064f579361612e76e2
kathy_chell <chela.lara76@gmail.com>
2024-10-18 17:05:35 -0600

    commits#1

--- EjercicioClase/Program.cs
+++ EjercicioClase/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using EjercicioClase;
+
+SuperHeroe superHeroe = new SuperHeroe();
+superHeroe.Nombre = "";
+superHeroe.IdentidadSecreta = "";
+superHeroe.ciudad = "";
+superHeroe.puedeVolar = "";
+
+Console.WriteLine($"Nombre: {superHeroe.Nombre}");
+Console.WriteLine($"Nombre: {superHeroe.IdentidadSecreta}");
+Console.WriteLine($"Nombre: {superHeroe.ciudad}");
+Console.WriteLine("Nombre:"+ superHeroe.ciudad+"?");
+
+SuperHeroe superHeroe1 = new SuperHeroe();
+superHeroe1.Nombre = "";
+superHeroe1.IdentidadSecreta = "";
+superHeroe1.ciudad = "";
+superHeroe1.puedeVolar = "";
+
+SuperHeroe superHeroe2 = new SuperHeroe();
+superHeroe2.Nombre = "";
+superHeroe2.IdentidadSecreta = "";
+superHeroe2.ciudad = "";
+superHeroe2.puedeVolar = "";
---
+++ EjercicioClase/SuperHeroe.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace EjercicioClase
+{
+    public class SuperHeroe
+    {
+        public string Nombre { get; set; }
+        public string IdentidadSecreta { get; set; }
+        public string ciudad {  get; set; }
+        public string puedeVolar {  get; set; }
+
+    }
+}
